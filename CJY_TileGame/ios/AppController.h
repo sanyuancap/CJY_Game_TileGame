e7261c603035faf7a5fb5fb07e551c7f5b6e5888
sanyuancap <sanyuancap@foxmail.com>
2014-01-21 15:46:47 -0800

    Initial Commit

---
+++ CJY_TileGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CJY_TileGameAppController.h
+//  CJY_TileGame
+//
+//  Created by CC on 14-1-21.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CJY_TileGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CJY_TileGameAppController.h
+//  CJY_TileGame
+//
+//  Created by CC on 14-1-21.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
