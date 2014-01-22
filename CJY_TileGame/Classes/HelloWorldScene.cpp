4c6ebf9ffaadc2b3647f178d9e457783def3a482
sanyuancap <sanyuancap@foxmail.com>
2014-01-22 15:55:11 -0800

    CJY_TileGame
    first test game about tile map

--- CJY_TileGame/Classes/HelloWorldScene.cpp
+++ CJY_TileGame/Classes/HelloWorldScene.cpp
@@ -4,6 +4,17 @@
 using namespace cocos2d;
 using namespace CocosDenshion;
 
+HelloWorld::HelloWorld(){
+    _tileMap = NULL;
+    _background = NULL;
+    _player = NULL;
+    _meta = NULL;
+    _foreground = NULL;
+    
+    _numCollected = 0;
+    _hud = NULL;
+}
+
 CCScene* HelloWorld::scene()
 {
     // 'scene' is an autorelease object
@@ -15,6 +26,13 @@ CCScene* HelloWorld::scene()
     // add layer as a child to scene
     scene->addChild(layer);
 
+    
+    /*Hud*/
+    HelloWorldHud *hud = HelloWorldHud::create();
+    scene->addChild(hud);
+    layer->setHud(hud);
+    
+    
     // return the scene
     return scene;
 }
@@ -22,56 +40,161 @@ CCScene* HelloWorld::scene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !CCLayer::init() )
-    {
-        return false;
+    /*add tilemap*/
+
+    bool bRet = false;
+    do{
+        CC_BREAK_IF(!CCLayer::init());
+        
+        this->setTileMap(CCTMXTiledMap::create("firsttest_cjy.tmx"));
+        this->setBackground(_tileMap->layerNamed("Background"));
+        
+        this->setMeta(_tileMap->layerNamed("Meta"));
+        _meta->setVisible(false);
+        
+        this->setForeground(_tileMap->layerNamed("Foreground"));
+        
+        this->addChild(_tileMap,-1);
+        bRet = true;
+    
+    }while (0);
+    
+    /*add objects*/
+    
+    CCTMXObjectGroup *objects = _tileMap->objectGroupNamed("Objects");
+    CCAssert(objects != NULL, "Objects' object group not found");
+    CCDictionary *spawnPoint = objects->objectNamed("SpawnPoint");
+    CCAssert(spawnPoint != NULL, "SpawnPoint object not found");
+    
+    int x = spawnPoint->valueForKey("x")->intValue();
+    int y = spawnPoint->valueForKey("y")->intValue();
+    this->setPlayer(CCSprite::create("Player.png"));
+    _player->setPosition(ccp(x, y));
+    this->addChild(_player);
+    this->setViewpointCenter(_player->getPosition());
+    
+    /*add move*/
+    this->setTouchEnabled(true);
+    
+    /*add music*/
+    CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadEffect("pickup.wav");
+    CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadEffect("hit.wav");
+    CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadEffect("move.wav");
+    
+    CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadBackgroundMusic("TileMap.wav");
+    CocosDenshion::SimpleAudioEngine::sharedEngine()->playBackgroundMusic("TileMap.wav",true);
+    
+    
+    
+    return bRet;
+}
+
+void HelloWorld::setViewpointCenter(cocos2d::CCPoint position){
+
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    
+    int x = MAX(position.x, winSize.width / 2);
+    int y = MAX(position.y, winSize.height / 2);
+    
+    x = MIN(x, (_tileMap->getMapSize().width * _tileMap->getTileSize().width - winSize.width / 2));
+    y = MIN(y, (_tileMap->getMapSize().height * _tileMap->getTileSize().height - winSize.width / 2));
+    
+    CCPoint actualPotision = ccp(x,y);
+    CCPoint centerOfView = ccp(winSize.width / 2,winSize.height / 2);
+    CCPoint viewPoint = ccpSub(centerOfView, actualPotision);
+    
+    this->setPosition(viewPoint);
+
+}
+
+void HelloWorld::registerWithTouchDispatcher(void){
+    
+    CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, 0, true);
+    
+}
+
+bool HelloWorld::ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){
+
+    return true;
+}
+
+void HelloWorld::ccTouchEnded(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){
+    
+    CCPoint touchLocation = this->convertTouchToNodeSpace(pTouch);
+    CCPoint playerPos = _player->getPosition();
+    CCPoint diff = ccpSub(touchLocation, playerPos);
+    
+    if (abs(diff.x) > abs(diff.y)) {
+        if (diff.x > 0) {
+            playerPos.x += _tileMap->getTileSize().width;
+            
+        }else{
+            
+            playerPos.x -= _tileMap->getTileSize().width;
+        }
+    }else{
+    
+        if (diff.y > 0) {
+            playerPos.y += _tileMap->getTileSize().height;
+        }else{
+        
+            playerPos.y -= _tileMap->getTileSize().height;
+        }
+    }
+    
+    if (playerPos.x <= (_tileMap->getMapSize().width * _tileMap->getTileSize().width) &&
+        playerPos.y <= (_tileMap->getMapSize().height * _tileMap->getTileSize().height) &&
+        playerPos.x >= 0 &&
+        playerPos.y >= 0) {
+        setPlayerPosition(playerPos);
     }
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+    setViewpointCenter(_player->getPosition());
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback) );
-    pCloseItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20) );
+}
 
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition( CCPointZero );
-    this->addChild(pMenu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Thonburi", 34);
-
-    // ask director the window size
-    CCSize size = CCDirector::sharedDirector()->getWinSize();
-
-    // position the label on the center of the screen
-    pLabel->setPosition( ccp(size.width / 2, size.height - 20) );
-
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    pSprite->setPosition( ccp(size.width/2, size.height/2) );
-
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
+void HelloWorld::setPlayerPosition(cocos2d::CCPoint position){
     
-    return true;
+    CCPoint tileCoord = this->tileCoordForPosition(position);
+    int tileGid = _meta->tileGIDAt(tileCoord);
+    if (tileGid) {
+        CCDictionary *properties = _tileMap->propertiesForGID(tileGid);
+        if (properties) {
+            /*walktable*/
+            const CCString *collision = properties->valueForKey("Collidable");
+            if (collision && collision->compare("true") == 0) {
+                /*music*/
+                CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("hit.wav");
+                return;
+            }
+            
+            /*eatable*/
+            const CCString *collectable = properties->valueForKey("Collectable");
+            if (collectable && collectable->compare("true") == 0) {
+                _meta->removeTileAt(tileCoord);
+                _foreground->removeTileAt(tileCoord);
+                
+                /*score*/
+                _numCollected++;
+                _hud->numCollectedChanged(_numCollected);
+                
+                /*music*/
+                CocosDenshion::SimpleAudioEngine::sharedEngine()->playEffect("pickup.wav");
+                
+            }
+        }
+    }
+    
+    
+    _player->setPosition(position);
+}
+
+CCPoint HelloWorld::tileCoordForPosition(cocos2d::CCPoint position){
+
+    int x = position.x / _tileMap->getTileSize().width;
+    int y = ((_tileMap->getMapSize().height * _tileMap->getTileSize().height) - position.y) / _tileMap->getTileSize().height;
+    
+    return ccp(x,y);
 }
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
@@ -82,3 +205,74 @@ void HelloWorld::menuCloseCallback(CCObject* pSender)
     exit(0);
 #endif
 }
+
+bool HelloWorldHud::init(){
+
+    bool bRet = false;
+    do{
+    
+        CC_BREAK_IF(!CCLayer::init());
+        
+        CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+        lable = CCLabelTTF::create("0","Verdana-Bold",18.0,CCSizeMake(50, 20),kCCTextAlignmentRight);
+        lable->setColor(ccc3(0, 0, 0));
+        int margin = 10;
+        lable->setPosition(ccp(winSize.width - (lable->getContentSize().width / 2) - margin,
+                               lable->getContentSize().height / 2 + margin));
+        this->addChild(lable);
+        bRet = true;
+    }while (0);
+    
+    return bRet;
+}
+
+void HelloWorldHud::numCollectedChanged(int numCollected){
+
+    lable->setString(CCString::createWithFormat("%d",numCollected)->getCString());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CJY_TileGame/Classes/HelloWorldScene.h
+++ CJY_TileGame/Classes/HelloWorldScene.h
@@ -3,12 +3,24 @@
 
 #include "cocos2d.h"
 
+/*score*/
+class HelloWorldHud : public cocos2d::CCLayer{
+
+public:
+    virtual bool init();
+    CREATE_FUNC(HelloWorldHud);
+    void numCollectedChanged(int numCollected);
+    cocos2d::CCLabelTTF *lable;
+};
+
+
 class HelloWorld : public cocos2d::CCLayer
 {
+
+
 public:
     // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
     virtual bool init();
-
     // there's no 'id' in cpp, so we recommend to return the class instance pointer
     static cocos2d::CCScene* scene();
     
@@ -17,6 +29,55 @@ public:
 
     // preprocessor macro for "static create()" constructor ( node() deprecated )
     CREATE_FUNC(HelloWorld);
+    
+    
+    /*cjy*/
+    CC_SYNTHESIZE_RETAIN(cocos2d::CCTMXTiledMap*, _tileMap, TileMap);
+    CC_SYNTHESIZE_RETAIN(cocos2d::CCTMXLayer*, _background, Background);
+    CC_SYNTHESIZE_RETAIN(cocos2d::CCSprite*, _player, Player);
+    CC_SYNTHESIZE_RETAIN(cocos2d::CCTMXLayer*, _meta, Meta);
+    CC_SYNTHESIZE_RETAIN(cocos2d::CCTMXLayer*, _foreground, Foreground);
+    CC_SYNTHESIZE(int, _numCollected, NumCollected);
+    CC_SYNTHESIZE_RETAIN(HelloWorldHud*, _hud, Hud);
+    
+    
+    HelloWorld();
+    void setViewpointCenter(cocos2d::CCPoint position);
+    
+    void registerWithTouchDispatcher(void);
+    
+    bool ccTouchBegan(cocos2d::CCTouch *pTouch,cocos2d::CCEvent *pEvent);
+    void ccTouchEnded(cocos2d::CCTouch *pTouch,cocos2d::CCEvent *pEvent);
+    
+  
+    
+    void setPlayerPosition(cocos2d::CCPoint position);
+    cocos2d::CCPoint tileCoordForPosition(cocos2d::CCPoint position);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
